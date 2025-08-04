ec30c5fc751166e2ac2c0b9286aa9558ee15e464
MatheusBorsa <matheusalek485@icloud.com>
2025-08-04 18:04:58 -0300

    feat: api service for fetching artists

---
+++ app/Http/Controllers/ArtistsController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\Http;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ArtistsController extends Controller
+{
+
+}
---
+++ app/Services/MusicBrainzService.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Services;
+
+use Illuminate\Support\Facades\Http;
+
+class MusicBrainzService
+{
+    protected string $baseUri = 'https://musicbrainz.org/ws/2/';
+    protected string $userAgent;
+
+    public function __construct()
+    {
+        $this->userAgent = config('services.musicbrainz.user_agent');
+    }
+
+    public function searchArtists(string $query, int $limit = 10): array
+    {
+        $response = Http::withHeaders([
+            'User-Agent' => $this->userAgent
+        ])->get($this->baseUri . 'artist/', [
+            'query' => $query,
+            'fmt' => 'json',
+            'limit' => $limit
+        ]);
+
+        return $response->json()['artists'] ?? [];
+    }
+
+        public function getArtist(string $mbid): array
+    {
+        $response = Http::withHeaders([
+            'User-Agent' => $this->userAgent
+        ])->get($this->baseUri . "artist/{$mbid}", [
+            'fmt' => 'json',
+            'inc' => 'url-rels+release-groups'
+        ]);
+
+        return $response->json();
+    }
+}
