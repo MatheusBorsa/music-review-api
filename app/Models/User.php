e614a38752789ea171fdea96effba02e6625ee27
MatheusBorsa <matheusalek485@icloud.com>
2025-08-04 19:37:37 -0300

    feat: pivot model

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use App\Models\UserArtist;
 
 class User extends Authenticatable
 {
@@ -47,5 +48,10 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function favoriteArtists()
+    {
+        return $this->hasMany(UserArtist::class);
     }
 }
